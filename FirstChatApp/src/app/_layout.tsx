4a9e4ce28051f40051c19125e12ef068fee836b2
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2024-10-07 21:40:59 +0300

    installed the getstream sdk

--- FirstChatApp/src/app/_layout.tsx
+++ FirstChatApp/src/app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack, Slot } from "expo-router";
-
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 
 //define global providers
-export default function RootLayout(){
-    return(
+
+export default function App() {
+    return <GestureHandlerRootView style={{ flex: 1 }}>
         <Slot/>
-    )
-}
+    </GestureHandlerRootView>;
+  }
+
+// export default function RootLayout(){
+//     return(
+//         <Slot/>
+//     )
+// }
