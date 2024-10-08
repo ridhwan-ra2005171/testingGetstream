59587ea7cdc3e9b090327f67824c106cdb06103a
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2024-10-08 20:34:00 +0300

    chat with specific channel added
    user can now chat with each others

--- FirstChatApp/src/components/UserListItem.tsx
+++ FirstChatApp/src/components/UserListItem.tsx
@@ -1,14 +1,35 @@
-import { Text, View } from 'react-native'
+import { Text, View, Pressable } from 'react-native'
 import React from 'react'
+import { useChatContext } from 'stream-chat-expo'
+import { useAuth } from '../providers/AuthProvider'
+import { router } from 'expo-router'
 
 const UserListItem = ({user}: {user: any}) => {
 
-    console.log("user in userlistitem:", user);
+    // console.log("user in userlistitem:", user);
+    const {client} = useChatContext()
+    //grab current user
+    const {user: currentUser} = useAuth()
+    const onPress = async ()=>{
+        //start a chat with that person
+        //use the client from useChatContext
+        //creating configuration of the channel
+        const channel = client.channel('messaging', {
+            members: [currentUser?.id, user.id]
+        })
+
+        await channel.watch();//create + watch for updates
+        //go to channel
+        router.replace(`/(home)/channel/${channel.cid}`)
+    }
     
   return (
-    <View style={{padding: 15, backgroundColor: 'white'}}>
+    <Pressable
+        onPress={onPress}
+        style={{padding: 15, backgroundColor: 'white'}}
+    >
       <Text style={{fontWeight: '600'}}>{user.full_name}</Text>
-    </View>
+    </Pressable>
   )
 }
 
